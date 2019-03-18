a8b51ac8444830e00a85d59ee4f56a45d9d90f35
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2019-03-18 06:58:22 -0400

    continuing dynamic coursework

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       {name: "Larry", age: 42},
       {name: "Curly", age: 24},
       {name: "Moe", age: 56}
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -25,54 +26,92 @@ class App extends Component {
     })
   }
 
+  deletePersonHandler = (personIndex) => {
+    const person = this.state.persons;
+
+  }
+
   nameChangedHandler = (event) => {
     this.setState({
       persons:[
-      {name: "Larry", age: 42},
+      {name: event.target.value, age: 42},
       {name: event.target.value, age: 24},
       {name: "Moe", age: 56}
       ]
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons:!doesShow})
+
+    // this.state.showPersons ? 
+    //   this.setState({showPersons: true}) 
+    //   :
+    //   this.setState({showPersons: false})
+
+    // if (this.state.showPersons === false) {
+    //   this.setState({showPersons: true})
+    // } else {
+    //   this.setState({showPersons: false})
+    // }
+  }
+
   render() {
 
-  const styling = {
-    backgroundColor: '#ccc',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '0.5px',
-    borderRadius: '3px',
-    cursor: 'pointer'
-  }
-    return (
-      <div className="App">
-      <h1>This is the same React App</h1>
-      <p>This is really working!!! 2</p>
-      <button 
-      style={styling}
-      onClick={() => this.switchNameHandler("Jose")}>Switch Name</button>
-          {/* {this.state.persons.map(person => {
-            return (
-              <Person 
-              name= {person.name}
-            )}
-          )} */}
-        
-          <Person 
+    const styling = {
+      backgroundColor: '#ccc',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '0.5px',
+      borderRadius: '3px',
+      cursor: 'pointer'
+    }
+    // because the re-render works through the whole render() method, persons is repeatedly set to null. this.state.showPersons is not reset, so the following if statem,ent works perfectly, and effeciently, and is best practices
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) =>  {
+            return <Person 
+              click = {() => this.deletePersonHandler(index)}
+              name = {person.name}
+              age = {person.age}
+            />
+
+          })}
+          {/* <Person 
             name={this.state.persons[0].name}
-            age={this.state.persons[0].age}/>
+            age={this.state.persons[0].age}
+            changed={this.nameChangedHandler}
+          />
           <Person 
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "William")}
+            // click={this.switchNameHandler.bind(this, "William")}
             changed={this.nameChangedHandler}>
-            My hobby: Piano</Person>
+            My hobby: Piano
+          </Person>
           <Person 
             name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/>
-      </div>
-    );
+            age={this.state.persons[2].age}
+            // changed={this.nameChangedHandler}
+          />  */}
+        </div>
+      );
+    }
+    return(
+      <div className="App">
+        <h1>This is the same React App</h1>
+        <p>This is really working!!! 2</p>
+        <button 
+          style={styling}
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
+      </div> 
+    )
   }
 }
 
